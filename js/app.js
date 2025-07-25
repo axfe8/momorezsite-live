fac77193c3274dbb726ed55558e9797d627909e1
axfe8 <egebaransen8@gmail.com>
2025-07-25 20:25:45 +0300

    hadi bakalım

--- js/app.js
+++ js/app.js
@@ -6,6 +6,7 @@ const phoneInput = document.getElementById('phoneNumber');
 const iti = window.intlTelInput(phoneInput, {
   initialCountry: "auto",
   separateDialCode: true,
+  autoHideDialCode: false,
   dropdownContainer: document.body,
   geoIpLookup: callback => {
     fetch('https://ipapi.co/json')
