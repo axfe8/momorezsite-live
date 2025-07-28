7d90fd01fd36df58c850906355f27a795223d8cd
Ege Baran SEN <egebaransen8@gmail.com>
2025-07-28 13:29:20 +0300

    misafir bilgilerinin gelmemesi

--- netlify/functions/submit.js
+++ netlify/functions/submit.js
@@ -76,7 +76,7 @@ exports.handler = async function(event) {
     return {
       statusCode: 200,
       headers: { "Content-Type": "application/json" },
-      body: JSON.stringify({ requestId: newId })
+      body: JSON.stringify({ requestId: newId, guests })
     };
 
   } catch (err) {
